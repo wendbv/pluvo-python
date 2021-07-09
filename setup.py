95dcc2829592257b0c2c2133c7b23b926b7d76a9
Lieuwe Mosch <lieuwemo@gmail.com>
2021-07-09 10:45:45 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='pluvo',
     packages=['pluvo'],
     package_data={},
-    version='0.2.17',
+    version='0.2.18',
     description='Python library to access the Pluvo REST API.',
     author='Wend BV',
     author_email='info@wend.nl',
